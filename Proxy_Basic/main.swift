c0470a998de78726c1688eb5194185dfceca7591
soheil <soheilhossein@SoheilHosseins-MacBook-Pro.local>
2017-12-29 03:33:31 +0330

    Initial Commit

---
+++ Proxy_Basic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Proxy_Basic
+//
+//  Created by SoheilHossein on 12/29/17.
+//  Copyright © 2017 SoheilHossein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
